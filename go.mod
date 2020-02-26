8b3c8da96977a17f11ffb2bb129f9fe0a5b6a024
Daishan <daishan@rancher.com>
2020-02-26 13:48:11 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/StrongMonkey/filewalk
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("need exactly two arguments")
+		os.Exit(1)
+	}
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	for _, r := range Walk(file) {
+		fmt.Println(r)
+	}
+}
+
+func Walk(f *os.File) []string {
+	var r []string
+	base := f.Name()
+	info, err := os.Stat(f.Name())
+	if err != nil {
+		panic(err)
+	}
+	if info.IsDir() {
+		files, err := f.Readdir(-1)
+		if err != nil {
+			panic(err)
+		}
+		for _, cf := range files {
+			childFile, err := os.Open(filepath.Join(f.Name(), cf.Name()))
+			if err != nil {
+				panic(err)
+			}
+			r1 := Walk(childFile)
+			for i, s := range r1 {
+				r1[i] = base + "/" + s
+			}
+			r = append(r, r1...)
+		}
+	} else {
+		r = append(r, base+"/"+basename(f))
+	}
+	return r
+}
+
+func basename(f *os.File) string {
+	return filepath.Base(f.Name())
+}
